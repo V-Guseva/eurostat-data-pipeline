1ea1714bbcecc9b29efe2fccefd81b3d8ee5d5b4
V-Guseva <variaguseva@gmail.com>
2025-09-29 14:29:28 +0300

    Added script to create table eurostat_ds

---
+++ infra/docker/postgres/init/03-create_eurostat_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS
+staging.eurostat_ds (
+code text primary key,
+title text,
+last_update_timestamp TIMESTAMPTZ NOT NULL,
+last_structure_change_timestamp TIMESTAMPTZ NOT NULL,
+start_year NUMERIC(4),
+end_year NUMERIC(4),
+period text,
+load_date DATE default CURRENT_DATE);
